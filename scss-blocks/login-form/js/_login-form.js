fe8c39a5b28dfdf43e48645c115913d62a4865f9
Alex Hrabovyi <alexgraboviypro@gmail.com>
2022-06-16 16:56:11 +0300

    Add login-form

---
+++ scss-blocks/login-form/js/_login-form.js
@@ -0,0 +1,7 @@
+new Modal({
+    modal_selector: ".login-form-modal",
+    toggle_button_selector: '[data-toggle-form="login-form"]',
+    hide_class: "hidden",
+    modal_class: "modal-window",
+    animation_class: "animation-translateY",
+});
