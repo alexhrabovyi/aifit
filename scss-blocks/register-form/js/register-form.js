ae6de8dc8bec702ece27b21d0c65a23219724cad
Alex Hrabovyi <alexgraboviypro@gmail.com>
2022-06-18 21:25:20 +0300

    Add additional verif for register form and AJAX

--- scss-blocks/register-form/js/register-form.js
+++ scss-blocks/register-form/js/register-form.js
@@ -82,15 +82,25 @@
 
     const buttonSubmit = registerForm.querySelector("[data-button-submit]");
 
+    let inputsStatus = {
+        nick: true,
+        passwordMain: true,
+        passwordConfirm: true,
+    }
+
     inputNick.addEventListener("input", (e) => {
-        e.target.value = e.target.value.replace(/[^a-zа-яёіїє'_1-9]/gi, "")
+        e.target.value = e.target.value.replace(/[^a-zа-яёіїє'_0-9]/gi, "")
 
         if (e.target.value.length > 30 || e.target.value.length < 3) {
             formErrorBlock.innerHTML = "Длина ника должна быть от 3 до 30 символов. Можно использовать латинские и кириллические буквы, цифры, апостроф и символ подчеркивания (_)";
             inputNick.classList.add("input_incorrect");
+
+            inputsStatus.nick = "error";
         } else {
             formErrorBlock.innerHTML = "";
             inputNick.classList.remove("input_incorrect");
+
+            inputsStatus.nick = true;
         }
     })
 
@@ -102,14 +112,18 @@
     })
 
     inputPasswordMain.addEventListener("input", (e) => {
-        e.target.value = e.target.value.replace(/[^a-z_1-9!@#$%^&*]/gi, "")
+        e.target.value = e.target.value.replace(/[^a-z_0-9!@#$%^&*]/gi, "")
 
         if (e.target.value.length > 30 || e.target.value.length < 8) {
             formErrorBlock.innerHTML = "Длина пароля должна быть от 8 до 30 символов. Пароль может содержать латинские буквы, символ подчеркивания, а также символы !@#$%^&*";
             inputPasswordMain.classList.add("input_incorrect");
+
+            inputsStatus.passwordMain = "error";
         } else {
             formErrorBlock.innerHTML = "";
             inputPasswordMain.classList.remove("input_incorrect");
+
+            inputsStatus.passwordMain = true;
         }
     })
 
@@ -121,14 +135,18 @@
     })
 
     inputPasswordConfirm.addEventListener("input", (e) => {
-        e.target.value = e.target.value.replace(/[^a-z_1-9!@#$%^&*]/gi, "")
+        e.target.value = e.target.value.replace(/[^a-z_0-9!@#$%^&*]/gi, "")
 
         if (e.target.value.length > 30 || e.target.value.length < 8) {
             formErrorBlock.innerHTML = "Длина пароля должна быть от 8 до 30 символов. Пароль может содержать латинские буквы, символ подчеркивания, а также символы !@#$%^&*";
             inputPasswordConfirm.classList.add("input_incorrect");
+
+            inputsStatus.passwordConfirm = "error";
         } else {
             formErrorBlock.innerHTML = "";
             inputPasswordConfirm.classList.remove("input_incorrect");
+
+            inputsStatus.passwordConfirm = true;
         }
     })
 
@@ -150,13 +168,75 @@
         let regExp = /^([1-9]|[1-9][0-9])$/;
 
         if (!regExp.test(e.target.value)) {
-            e.target.value = e.target.value.replace(/([1-9])(\d)/g, (match, p1, p2) => p1);
+            e.target.value = e.target.value.replace(/([1-9])(\d)/g, (match, p1) => p1);
         }
     })
 
     buttonSubmit.addEventListener("click", (e) => {
         e.preventDefault();
+
+        if (inputsStatus.nick === "error" || inputNick.value.length === 0) {
+            inputNick.focus();
+            return;
+        } else if (inputsStatus.passwordMain === "error" || inputPasswordMain.value.length === 0) {
+            inputPasswordMain.focus();
+            return;
+        } else if (inputsStatus.passwordConfirm === "error" || inputPasswordConfirm.value.length === 0) {
+            inputPasswordConfirm.focus();
+            return;
+        } else if (inputAge.value.length === 0) {
+            inputAge.focus();
+            return;
+        }
+
+        formErrorBlock.innerHTML = "";
+
+        if (inputPasswordMain.value !== inputPasswordConfirm.value) {
+            inputPasswordConfirm.classList.add("input_incorrect");
+
+            formErrorBlock.innerHTML = "Пароли не совпадают";
+
+            inputPasswordConfirm.focus();
+
+            return;
+        }
+
+        let body = {
+            "nickname": inputNick.value,
+            "password": inputPasswordMain.value,
+            "age": inputAge.value,
+        }
+
+        if (checkBoxBlock.dataset.selectedId === "0") {
+            body.gender = "m";
+        } else {
+            body.gender = "f";
+        }
+
+        console.log(body);
+
+        body = JSON.stringify(body);
+
+        buttonSubmit.style.cssText = "background: #FFFFFF; border: 1px solid #0B98FF";
+        buttonSubmit.innerHTML = '<svg class="circular-loader"viewBox="25 25 50 50" ><circle class="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" stroke-width="2" /></svg>'
+
+        document.body.style.pointerEvents = "none";
+
+        async function postData(url, data) {
+            let res = await fetch(url, {
+                method: "POST",
+                body: data,
+            })
+
+            return await res.text();
+        }
+
+        postData("http://localhost:8080/registration", body)
+            .then((res) => {
+                console.log(res);
+                buttonSubmit.innerHTML = "Готово";
+                buttonSubmit.style.color = "#0B98FF";
+            })
     })
 }
-
 
